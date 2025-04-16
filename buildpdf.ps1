d7dd4d14768fc5056b0d19f85172e4683057b06e
Mao Hongyu <140867647+maohy0@users.noreply.github.com>
2025-04-16 22:09:20 +0800

    Update buildpdf.ps1

--- buildpdf.ps1
+++ buildpdf.ps1
@@ -1,5 +1,6 @@
 # 编译tex文件
 xelatex main.tex
+xelatex main.tex
 
 # 删除编译产生的临时文件
 $confirmation = Read-Host -Prompt "Are you sure to delete temporary files? If no, you could press Ctrl+C to interrupt"
